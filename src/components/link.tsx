46db2fef993a549c3cfb2fbc2a0eab69cc782b56
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-05 20:46:57 +0100

    Init config

---
+++ src/components/link.tsx
@@ -0,0 +1,13 @@
+import * as Headless from "@headlessui/react";
+import React, { forwardRef } from "react";
+import NextLink, { LinkProps } from "next/link";
+export const Link = forwardRef(function Link(
+  props: LinkProps & React.ComponentPropsWithoutRef<"a">,
+  ref: React.ForwardedRef<HTMLAnchorElement>
+) {
+  return (
+    <Headless.DataInteractive>
+      <NextLink {...props} ref={ref} />
+    </Headless.DataInteractive>
+  );
+});
