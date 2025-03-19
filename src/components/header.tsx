19a3886aac3d40a52aa6bbb05ec403c77d958911
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-19 20:31:49 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -54,7 +54,7 @@ export const Header = () => {
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background pr-4 sm:ring-1 sm:ring-zinc-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-4sm:ring-1 sm:ring-zinc-900/10">
           <div className="flex items-center justify-between h-[60px] px-4">
             <Link href="#">
               <span className="sr-only">Spline</span>
@@ -67,7 +67,7 @@ export const Header = () => {
             <Button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 cursor-pointer"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 cursor-pointer"
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon aria-hidden="true" className="size-6" />
