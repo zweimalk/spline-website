ca5bb075dfb7689be4a0954c4038c6701cbe0e50
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-28 06:09:41 +0100

    Add menu animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,10 @@
 
 import { Link } from '@/components/link';
 import ThemeToggler from '@/components/theme-toggler';
-import { Button, Dialog, DialogPanel } from '@headlessui/react';
+import { Button, Dialog, DialogPanel, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useTheme } from 'next-themes';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { ArrowRightSvgIcon, FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon, SplineLogo } from './icons';
 
 const mobileNavigation = [
@@ -54,60 +54,87 @@ export const Header = () => {
           <ThemeToggler />
         </div>
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className='lg:hidden'>
-        <div className='fixed inset-0 z-10' />
-        <DialogPanel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-4sm:ring-1 sm:ring-zinc-900/10'>
-          <div className='flex items-center justify-end h-[60px] px-4'>
-            <Button
-              type='button'
-              onClick={() => setMobileMenuOpen(false)}
-              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 cursor-pointer'
-            >
-              <span className='sr-only'>Close menu</span>
-              <XMarkIcon aria-hidden='true' className='size-6' />
-            </Button>
-          </div>
-          <div className='mt-8 flow-root px-8'>
-            <div className='flex flex-col gap-8'>
-              <div>
-                <p>Our services</p>
-                <div className='space-y-5 py-6 pl-12'>
-                  {mobileNavigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className='-mx-3 flex items-center justify-between max-w-[220px]'
+      <Transition appear show={mobileMenuOpen} as={Fragment}>
+        <Dialog as='div' className='relative z-50 lg:hidden' onClose={setMobileMenuOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black/50' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-end text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='transform transition ease-in-out duration-300'
+                enterFrom='translate-x-full'
+                enterTo='translate-x-0'
+                leave='transform transition ease-in-out duration-300'
+                leaveFrom='translate-x-0'
+                leaveTo='translate-x-full'
+              >
+                <DialogPanel className='w-full max-w-[400px] h-full fixed right-0 top-0 overflow-y-auto bg-background px-4 text-left align-middle shadow-xl'>
+                  <div className='flex items-center justify-end h-[60px]'>
+                    <Button
+                      type='button'
+                      onClick={() => setMobileMenuOpen(false)}
+                      className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 cursor-pointer'
                     >
-                      {item.name}
-                      <ArrowRightSvgIcon className='w-6 h-6' />
+                      <span className='sr-only'>Close menu</span>
+                      <XMarkIcon aria-hidden='true' className='size-6' />
+                    </Button>
+                  </div>
+                  <div className='mt-8 flow-root px-8'>
+                    <div className='flex flex-col gap-8'>
+                      <div>
+                        <p>Our services</p>
+                        <div className='space-y-5 py-6 pl-12'>
+                          {mobileNavigation.map((item) => (
+                            <Link
+                              key={item.name}
+                              href={item.href}
+                              className='-mx-3 flex items-center justify-between max-w-[220px]'
+                            >
+                              {item.name}
+                              <ArrowRightSvgIcon className='w-6 h-6' />
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                      <p>Useful documents</p>
+                      <p>Join Us</p>
+                      <p>Contact</p>
+                    </div>
+                  </div>
+                  <div className='mt-8 flow-root px-8'>
+                    <ThemeToggler />
+                  </div>
+                  <div className='flex ml-auto px-9 gap-4 mt-4'>
+                    <Link href='https://www.linkedin.com/company/spline-polska'>
+                      <span className='sr-only'>LinkedIn</span>
+                      <LinkedinLogoIcon className='w-5 h-5' />
                     </Link>
-                  ))}
-                </div>
-              </div>
-              <p>Useful documents</p>
-              <p>Join Us</p>
-              <p>Contact</p>
+                    <Link href='https://instagram.com/spline'>
+                      <span className='sr-only'>Instagram</span>
+                      <InstagramLogoIcon className='w-5 h-5' />
+                    </Link>
+                    <Link href='https://facebook.com/spline'>
+                      <span className='sr-only'>Facebook</span>
+                      <FacebookLogoIcon className='w-5 h-5' />
+                    </Link>
+                  </div>
+                </DialogPanel>
+              </Transition.Child>
             </div>
           </div>
-          <div className='mt-8 flow-root px-8'>
-            <ThemeToggler />
-          </div>
-          <div className='flex ml-auto px-9 gap-4 mt-4'>
-            <Link href='https://www.linkedin.com/company/spline-polska'>
-              <span className='sr-only'>LinkedIn</span>
-              <LinkedinLogoIcon className='w-5 h-5' />
-            </Link>
-            <Link href='https://instagram.com/spline'>
-              <span className='sr-only'>Instagram</span>
-              <InstagramLogoIcon className='w-5 h-5' />
-            </Link>
-            <Link href='https://facebook.com/spline'>
-              <span className='sr-only'>Facebook</span>
-              <FacebookLogoIcon className='w-5 h-5' />
-            </Link>
-          </div>
-        </DialogPanel>
-      </Dialog>
+        </Dialog>
+      </Transition>
     </header>
   );
 };
