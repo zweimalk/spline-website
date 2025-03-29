bf7acd4971e64f5619ff3b2dcdd387433a5c7f1e
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-29 19:33:31 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,19 +11,6 @@ const nextConfig: NextConfig = {
       'perceptive-quail-838.convex.cloud',
       'plus.unsplash.com',
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['motion/react', '@react-three/fiber', '@react-three/drei'],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
