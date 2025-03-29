a7fd188f7fed24e097665abc0cbc16d46fd56041
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-29 13:54:56 +0100

    Update

--- src/components/quotes-slider.tsx
+++ src/components/quotes-slider.tsx
@@ -59,18 +59,26 @@ export const QuotesSlider = ({ quotes, autoPlayInterval = 5000, className }: Quo
       // onMouseEnter={handleMouseEnter}
       // onMouseLeave={handleMouseLeave}
     >
-      <div className='flex flex-col lg:flex-row items-center gap-8 lg:gap-16 p-6 lg:p-12 rounded-lg'>
+      <div className='flex flex-col lg:grid lg:grid-cols-2 items-center gap-8 lg:gap-16 p-6 lg:p-12 rounded-lg'>
         {/* Image section */}
-        <div className='flex justify-center md:mt-6'>
+        <div className='flex justify-center md:mt-6 lg:mt-10 lg:justify-start w-full'>
           <div className='relative w-55 h-80 overflow-hidden rounded-lg clip-diagonal clip-diagonal-sm'>
             <Image src={quotes[activeIndex].imageSrc} alt={quotes[activeIndex].name} fill className='object-cover' />
+          </div>
+          <div className='hidden lg:block w-40 ml-4'>
+            <Header3 className='font-semibold'>{quotes[activeIndex].name}</Header3>
+            <p className=' text-gray-4 dark:text-gray-2 tracking-wider'>{quotes[activeIndex].role}</p>
           </div>
         </div>
 
         {/* Content section */}
-        <div className='w-full '>
-          <Header3 className='text-center text-2xl lg:text-3xl font-semibold'>{quotes[activeIndex].name}</Header3>
-          <p className='text-center text-lg text-gray-4 dark:text-gray-2 tracking-wider'>{quotes[activeIndex].role}</p>
+        <div className='w-full'>
+          <div className='lg:hidden'>
+            <Header3 className='text-center text-2xl lg:text-3xl font-semibold'>{quotes[activeIndex].name}</Header3>
+            <p className='text-center text-lg text-gray-4 dark:text-gray-2 tracking-wider'>
+              {quotes[activeIndex].role}
+            </p>
+          </div>
 
           <div className='flex justify-center items-center my-4 gap-8 md:hidden'>
             <button onClick={prevSlide} aria-label='Previous testimonial'>
@@ -96,14 +104,14 @@ export const QuotesSlider = ({ quotes, autoPlayInterval = 5000, className }: Quo
             </button>
           </div>
 
-          <div className='md:max-w-[500px] mx-auto mt-10 relative'>
+          <div className='md:max-w-[500px] mx-auto mt-10 relative lg:mt-0'>
             <svg
               width='33'
               height='26'
               viewBox='0 0 33 26'
               fill='none'
               xmlns='http://www.w3.org/2000/svg'
-              className='hidden md:inline translate-y-8 -translate-x-4'
+              className='hidden md:inline translate-y-8 -translate-x-4 lg:-translate-x-12'
             >
               <path
                 d='M19.8481 25.728V14.4C19.8481 10.304 21.1281 7.04 23.6881 4.608C26.3121 2.112 29.4161 0.576 33.0001 0V4.512C31.2721 5.088 29.7361 6.144 28.3921 7.68C27.1121 9.216 26.4721 11.072 26.4721 13.248H32.8081V25.728H19.8481ZM0.840088 25.728V14.4C0.840088 10.304 2.12009 7.04 4.68009 4.608C7.30409 2.112 10.4081 0.576 13.9921 0V4.512C12.2641 5.088 10.7281 6.144 9.38409 7.68C8.10409 9.216 7.46409 11.072 7.46409 13.248H13.8001V25.728H0.840088Z'
@@ -111,10 +119,12 @@ export const QuotesSlider = ({ quotes, autoPlayInterval = 5000, className }: Quo
               />
             </svg>
 
-            <Cite className='text-center'>{quotes[activeIndex].quote}</Cite>
+            <Cite className='text-center lg:text-left'>{quotes[activeIndex].quote}</Cite>
           </div>
 
-          <Body1 className='text-center mt-12 md:max-w-[500px] mx-auto'>{quotes[activeIndex].description}</Body1>
+          <Body1 className='text-center mt-12 md:max-w-[500px] mx-auto lg:text-left lg:mt-2'>
+            {quotes[activeIndex].description}
+          </Body1>
         </div>
       </div>
 
