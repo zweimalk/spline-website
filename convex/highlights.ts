7e2f71618b096313ef987ce02964f3bffbbd77a0
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-31 20:18:08 +0200

    Add sorting to highlights

--- convex/highlights.ts
+++ convex/highlights.ts
@@ -1,11 +1,12 @@
-import { query } from "./_generated/server";
+import { query } from './_generated/server';
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    const highlights = await ctx.db.query("highlights").collect();
+    const highlights = await ctx.db.query('highlights').collect();
+    const sortedHighlights = highlights.sort((a, b) => b.year - a.year);
     return Promise.all(
-      highlights.map(async (highlight) => ({
+      sortedHighlights.map(async (highlight) => ({
         ...highlight,
         imageUrl: await ctx.storage.getUrl(highlight.imageUrl),
       }))
