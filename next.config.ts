97f638f9c2d28db4a62553f355a20cb834b94c2c
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-03-31 15:53:42 +0200

    Add conve urls

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
       'via.placeholder.com',
       'v5.airtableusercontent.com',
       'rosy-mink-728.convex.cloud',
+      'fantastic-malamute-299.convex.cloud',
+      'canny-blackbird-49.convex.cloud',
       'perceptive-quail-838.convex.cloud',
       'plus.unsplash.com',
     ],
