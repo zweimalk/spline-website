29cfc1ffca6ee3cf0779e6b8aa8fe727ebe5702d
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-04-12 09:59:01 +0200

    Add competence section

--- src/app/software-development/page.tsx
+++ src/app/software-development/page.tsx
@@ -1,7 +1,127 @@
+import { Accordion } from '@/components/accordion';
 import { ContactPeople } from '@/components/contact-people';
 import { Hero } from '@/components/hero';
+import { Body1 } from '@/components/Typo/Body1';
+import { Header3 } from '@/components/Typo/Header3';
+import { Title } from '@/components/Typo/Title';
 import Image from 'next/image';
 
+const items = [
+  {
+    title: 'DevOps',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Streamlining and automating development workflows</li>
+          <li>CI/CD pipeline implementation and infrastructure management</li>
+          <li>Containerization, orchestration, and configuration management for efficient deployment</li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Architecture',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Event driven architecture</li>
+          <li>Cloud architecture</li>
+          <li>Caching</li>
+          <li>Pub/Sub</li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Data management',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Data processing and management solutions, including ETL pipelines and data lakes</li>
+          <li>Enabling efficient data storage, retrieval, and analysis</li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Business analysis',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Data visualization, predictive modeling, and performance tracking</li>
+          <li>End-to-end analytics solutions for actionable insights</li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Service Desk',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Systems engineering</li>
+          <li>Design and project planning</li>
+          <li>Calculation and simulation</li>
+          <li>Programming</li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Development',
+    content: (
+      <>
+        <ul className='list-disc list-inside flex flex-col gap-2'>
+          <li>Front-end, back-end, and full-stack application development</li>
+          <li>User-friendly interfaces and cross-platform compatibility</li>
+          <li>
+            Robust and scalable software solutions tailored to client needspment and maintenance of embedded systems
+          </li>
+          <li>
+            Firmware design, integration, and optimization for reliable hardware performancecompetition. We understand
+            that organisations we work with may be at different points.
+          </li>
+        </ul>
+        <div className='mt-4'>
+          <Body1>Tech Stack</Body1>
+          <div className='flex flex-wrap gap-2'>
+            <Body1>Python</Body1>
+          </div>
+        </div>
+      </>
+    ),
+  },
+];
 export default function SoftwareDevelopment() {
   return (
     <>
@@ -12,7 +132,7 @@ export default function SoftwareDevelopment() {
           </>
         }
         description={
-          'We deliver future-proof, tailor-made solutions that support our clients&apos; day-to-day operations and help them gain an edge over their competition.'
+          'We create reliable, scalable, and efficient software solutions tailored to your business needs. From backend systems and cloud platforms to mobile and desktop applications, our development teams bring deep technical knowledge, agile processes, and a strong focus on quality to every project.'
         }
         mediaElement={
           <Image
@@ -24,6 +144,18 @@ export default function SoftwareDevelopment() {
           />
         }
       />
+      <div className='px-4 mt-20'>
+        <Header3>automotive & mobility</Header3>
+        <Title className='mt-4'>competence</Title>
+        <Body1 className='mt-8'>
+          We deliver future-proof, tailor-made solutions that support our clients&apos; day-to-day operations and help
+          them gain an edge over their competition. We understand that organisations we work with may be at different
+          points.
+        </Body1>
+        <div className='mt-8'>
+          <Accordion items={items} />
+        </div>
+      </div>
       <ContactPeople />
     </>
   );
---
+++ src/components/accordion.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
+import { AnimatePresence, easeOut, motion } from 'motion/react';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Body1 } from './Typo/Body1';
+
+interface AccordionItem {
+  title: string;
+  content: React.ReactNode;
+}
+
+interface AccordionProps {
+  items: AccordionItem[];
+}
+
+export const Accordion = ({ items }: AccordionProps) => {
+  const [isAnyOpen, setIsAnyOpen] = useState(false);
+
+  return (
+    <div className='mx-auto w-full max-w-lg rounded-xl'>
+      {items.map((item, index) => (
+        <Disclosure key={item.title + index}>
+          {({ open }) => {
+            // Update the open state when any disclosure changes
+            if (open !== isAnyOpen) {
+              setIsAnyOpen(open);
+            }
+            return (
+              <div key={item.title + index}>
+                <DisclosureButton className='flex w-full items-center justify-between border-b text-left py-4'>
+                  <Body1 className='text-2xl'>{item.title}</Body1>
+                  <svg
+                    width='23'
+                    height='23'
+                    viewBox='0 0 23 23'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    className={cn('h-6 w-6 transition-transform duration-300', open && 'rotate-90')}
+                  >
+                    <path
+                      d='M13.0833 3L21 11.5M21 11.5L13.0833 20M21 11.5L2 11.5'
+                      stroke='var(--foreground)'
+                      strokeWidth='3'
+                      strokeLinecap='square'
+                      strokeLinejoin='round'
+                    />
+                  </svg>
+                </DisclosureButton>
+                <AnimatePresence>
+                  <DisclosurePanel>
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      transition={{ duration: 0.5, ease: easeOut }}
+                      className='origin-top py-4'
+                    >
+                      {item.content}
+                    </motion.div>
+                  </DisclosurePanel>
+                </AnimatePresence>
+              </div>
+            );
+          }}
+        </Disclosure>
+      ))}
+      <div className='mt-8'>
+        <Image
+          src={'/images/software-development/shape.jpeg'}
+          alt={'Automotive & mobility'}
+          width={1000}
+          height={1000}
+          className={cn('w-full transition-all duration-300 aspect-[9/16] object-cover', isAnyOpen && 'aspect-[16/9] ')}
+        />
+      </div>
+    </div>
+  );
+};
