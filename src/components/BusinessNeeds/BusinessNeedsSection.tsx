ee1aa3df84ffd4ead8a72cdcc3883221d6c1447d
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-04-20 17:46:47 +0200

    Working on responsive slider.

--- src/components/BusinessNeeds/BusinessNeedsSection.tsx
+++ src/components/BusinessNeeds/BusinessNeedsSection.tsx
@@ -19,14 +19,17 @@ export const BusinessNeedsSection = ({ slides }: { slides: Slide[] }) => {
 
   return (
     <div className='px-4 mt-20 md:mt-40'>
-      <Title className=''>
-        business <br /> needs
-      </Title>
-      <Body1 className='mt-24 select-none'>
-        Choose your cooperation model - we offer adaptable cooperation models to align with your current needs.
-      </Body1>
-      <div className='border-1 border-foreground/30 rounded-lg mt-24 overflow-hidden'>
-        <div className='flex gap-2 p-4 justify-end border-b border-foreground/30'>
+      <div className='md:landscape:flex md:landscape:items-center md:landscape:justify-between'>
+        <Title className=''>
+          business <br /> needs
+        </Title>
+        <Body1 className='mt-24 select-none max-w-[65ch]'>
+          We tailor our cooperation models to match your project goals, internal capacity, and preferred level of
+          ownership—offering flexible, scalable, and effective engagement options.
+        </Body1>
+      </div>
+      <div className='border-1 border-foreground/30 rounded-lg mt-24 md:landscape:grid md:landscape:grid-cols-[1fr_200px] md:landscape:grid-rows-1 md:landscape:ml-auto md:landscape:max-w-[1400px]'>
+        <div className='flex flex-1 gap-2 p-4 justify-end border-b border-foreground/30 md:landscape:col-start-2 md:landscape:border-b-0 md:landscape:border-l'>
           <ArrowLeftIcon
             className={cn(
               'size-12 p-2 border border-foreground/30 rounded-full cursor-pointer',
@@ -50,17 +53,19 @@ export const BusinessNeedsSection = ({ slides }: { slides: Slide[] }) => {
             }}
           />
         </div>
-        <div className='px-[34px] pt-10 pb-16'>
-          <Cite>{slides[currentSlide].title}</Cite>
-          <Body1 className='mt-6'>{slides[currentSlide].description}</Body1>
+        <div className='md:landscape:col-start-1 md:landscape:grid-cols-2 md:landscape:row-start-1 md:landscape:grid md:landscape:grid-rows-1 h-full'>
+          <div className='px-[34px] pt-10 pb-16 md:col-start-2 md:landscape:pb-0'>
+            <Cite>{slides[currentSlide].title}</Cite>
+            <Body1 className='mt-6'>{slides[currentSlide].description}</Body1>
+          </div>
+          <Image
+            src={slides[currentSlide].imageUrl}
+            alt='Staff augmentation'
+            width={700}
+            height={700}
+            className='aspect-video object-cover md:col-start-1 w-full md:row-start-1 md:landscape:h-full'
+          />
         </div>
-        <Image
-          src={slides[currentSlide].imageUrl}
-          alt='Staff augmentation'
-          width={500}
-          height={500}
-          className='aspect-video object-cover'
-        />
       </div>
     </div>
   );
