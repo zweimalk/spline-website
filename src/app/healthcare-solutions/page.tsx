dd37a8ac5b5c3bc9ba8b8a84630c9dcc183040ad
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-05-04 12:26:29 +0200

    Update page.tsx

--- src/app/healthcare-solutions/page.tsx
+++ src/app/healthcare-solutions/page.tsx
@@ -1,34 +1,42 @@
+import { BusinessNeedsSection, type Slide } from '@/components/BusinessNeeds/BusinessNeedsSection';
 import { Body1 } from '@/components/Typo/Body1';
+import { Cite } from '@/components/Typo/Cite';
+import { ContactPeople } from '@/components/contact-people';
 import { Hero } from '@/components/hero';
+import { HighlightsSection } from '@/components/highlights-section';
+import type { Card } from '@/components/infinite-cards-scroll';
+import { SeeAlsoSection } from '@/components/see-also-section';
 import Image from 'next/image';
 import { CompetenceAccordion, type CompetenceAccordionItem } from '../software-development/competence-accordion';
+import type { HowWeWorkItem } from '../software-development/how-we-work-section';
+import { HowWeWorkSection } from '../software-development/how-we-work-section';
 
-// const slides: Slide[] = [
-//   {
-//     title: 'Staff augmentation',
-//     description:
-//       "At the heart of our approach is transparency. We're always in touch with stakeholders and make sure they're regularly informed about project progress. Our dedication to transparency extends to our reporting, which always provides clear insights into project developments and milestones. Reach out and within 48 hours, we'll have CVs of top devs who meet your requirements in your inbox.",
-//     imageUrl: '/images/software-development/business-needs/augmentation.webp',
-//   },
-//   {
-//     title: 'Team Extensions',
-//     description:
-//       'Expand your in-house team with experienced engineers who seamlessly integrate into your workflows, tools, and culture. This model gives you full control while accelerating delivery with specialized skills and reduced recruitment overhead.',
-//     imageUrl: '/images/software-development/business-needs/extensions.png',
-//   },
-//   {
-//     title: 'Dedicated Teams',
-//     description:
-//       'Access a full, cross-functional team focused entirely on your project. We assemble and manage a group tailored to your domain and requirements—ensuring aligned goals, efficient communication, and long-term value creation.',
-//     imageUrl: '/images/software-development/business-needs/teams.png',
-//   },
-//   {
-//     title: 'Managed Services',
-//     description:
-//       'Offload full responsibility for your project or product development. From planning and architecture to implementation and maintenance, we deliver end-to-end solutions with clear accountability, timelines, and outcomes.',
-//     imageUrl: '/images/software-development/business-needs/services.png',
-//   },
-// ];
+const slides: Slide[] = [
+  {
+    title: 'Staff augmentation',
+    description:
+      "At the heart of our approach is transparency. We're always in touch with stakeholders and make sure they're regularly informed about project progress. Our dedication to transparency extends to our reporting, which always provides clear insights into project developments and milestones. Reach out and within 48 hours, we'll have CVs of top devs who meet your requirements in your inbox.",
+    imageUrl: '/images/software-development/business-needs/augmentation.webp',
+  },
+  {
+    title: 'Team Extensions',
+    description:
+      'Expand your in-house team with experienced engineers who seamlessly integrate into your workflows, tools, and culture. This model gives you full control while accelerating delivery with specialized skills and reduced recruitment overhead.',
+    imageUrl: '/images/software-development/business-needs/extensions.png',
+  },
+  {
+    title: 'Dedicated Teams',
+    description:
+      'Access a full, cross-functional team focused entirely on your project. We assemble and manage a group tailored to your domain and requirements—ensuring aligned goals, efficient communication, and long-term value creation.',
+    imageUrl: '/images/software-development/business-needs/teams.png',
+  },
+  {
+    title: 'Managed Services',
+    description:
+      'Offload full responsibility for your project or product development. From planning and architecture to implementation and maintenance, we deliver end-to-end solutions with clear accountability, timelines, and outcomes.',
+    imageUrl: '/images/software-development/business-needs/services.png',
+  },
+];
 
 const compoetenceItems: CompetenceAccordionItem[] = [
   {
@@ -101,87 +109,72 @@ const compoetenceItems: CompetenceAccordionItem[] = [
   },
 ];
 
-// const cards: Card[] = [
-//   {
-//     title: (
-//       <Cite className='text-left text-white z-10'>
-//         technology <br /> & Electronics
-//       </Cite>
-//     ),
-//     imgAlt: 'Technology & Electronics',
-//     tags: ['IoT', 'embedded', 'firmware'],
-//     imageUrl: '/images/service-cards/technology_electronics.png',
-//   },
-//   {
-//     title: (
-//       <Cite className='text-left text-white z-10'>
-//         automotive <br /> & mobility
-//       </Cite>
-//     ),
-//     imgAlt: 'Automotive & Mobility',
-//     tags: ['surfacing', 'engineering', 'hmi'],
-//     imageUrl: '/images/service-cards/automotive.png',
-//   },
-//   {
-//     title: (
-//       <Cite className='text-left text-white z-10'>
-//         healthcare <br /> solutions
-//       </Cite>
-//     ),
-//     imgAlt: 'Healthcare Solutions',
-//     tags: ['medtech', 'AI', 'imaging'],
-//     imageUrl: '/images/service-cards/healthcare.png',
-//   },
-//   {
-//     title: (
-//       <Cite className='text-left text-white z-10'>
-//         retail media
-//         <br /> & services
-//       </Cite>
-//     ),
-//     imgAlt: 'Retail Media & Services',
-//     tags: ['ecommerce', 'fintech', 'automation'],
-//     imageUrl: '/images/service-cards/retail.png',
-//   },
-// ];
+const cards: Card[] = [
+  {
+    title: (
+      <Cite className='text-left text-white z-10'>
+        technology <br /> & Electronics
+      </Cite>
+    ),
+    imgAlt: 'Technology & Electronics',
+    tags: ['IoT', 'embedded', 'firmware'],
+    imageUrl: '/images/service-cards/technology_electronics.png',
+  },
+  {
+    title: (
+      <Cite className='text-left text-white z-10'>
+        automotive <br /> & mobility
+      </Cite>
+    ),
+    imgAlt: 'Automotive & Mobility',
+    tags: ['surfacing', 'engineering', 'hmi'],
+    imageUrl: '/images/service-cards/automotive.png',
+  },
+  {
+    title: (
+      <Cite className='text-left text-white z-10'>
+        software <br /> & development
+      </Cite>
+    ),
+    imgAlt: 'Software & Development',
+    tags: ['cloud', 'AI', 'backend'],
+    imageUrl: '/images/service-cards/mobility.png',
+  },
+  {
+    title: (
+      <Cite className='text-left text-white z-10'>
+        retail media
+        <br /> & services
+      </Cite>
+    ),
+    imgAlt: 'Retail Media & Services',
+    tags: ['ecommerce', 'fintech', 'automation'],
+    imageUrl: '/images/service-cards/retail.png',
+  },
+];
 
-// const howWeWorkItems: HowWeWorkItem[] = [
-//   {
-//     title: 'Discovery',
-//     content:
-//       'We begin by understanding your goals, challenges, and requirements to ensure every solution is aligned with your business needs.',
-//   },
-//   {
-//     title: 'Planning',
-//     content:
-//       'Our team defines the architecture, selects the right technologies, and outlines a scalable roadmap tailored to your project.',
-//   },
-//   {
-//     title: 'Team Assembly',
-//     content:
-//       'We build a dedicated team of specialists, matching skills and experience to your domain, goals, and timeline.',
-//   },
-//   {
-//     title: 'Solution Design',
-//     content:
-//       'We define the system structure, workflows, and technical approach —balancing performance, scalability, and maintainability based on project size and complexity.',
-//   },
-//   {
-//     title: 'Development & Integration',
-//     content:
-//       'Using agile methodologies, we deliver clean, high-quality code with frequent iterations, seamless integration, and continuous stakeholder feedback.',
-//   },
-//   {
-//     title: 'Testing & Quality Assurance',
-//     content:
-//       'Our rigorous QA processes—including automated and manual testing —ensure functionality, performance, and security at every stage.',
-//   },
-//   {
-//     title: 'Deployment & Monitoring',
-//     content:
-//       'We handle smooth releases, infrastructure setup, and post-launch monitoring to ensure system stability and performance from day one.',
-//   },
-// ];
+const howWeWorkItems: HowWeWorkItem[] = [
+  {
+    title: 'Discovery',
+    content:
+      'We begin by understanding your healthcare goals, regulatory requirements, and technical needs, ensuring full alignment with industry standards (e.g., ISO 13485, IEC 60601).',
+  },
+  {
+    title: 'Development',
+    content:
+      'Our experts develop robust solutions—designing both hardware and software, integrating systems, and ensuring compliance with medical device standards and regulations.',
+  },
+  {
+    title: 'Engineering',
+    content:
+      'Our teams handle circuit design, system configuration, and software development using platforms like Altium, Zuken, and proprietary OEM environments.',
+  },
+  {
+    title: 'Testing & Compliance',
+    content:
+      'We perform functional testing, EMC validation, and documentation according to IEC, ISO, and CE requirements, preparing your product for market readiness.',
+  },
+];
 
 export default function HealthcareSolutions() {
   return (
@@ -216,11 +209,11 @@ export default function HealthcareSolutions() {
         imageAlt={'Healthcare Solutions'}
       />
 
-      {/* <HowWeWorkSection howWeWorkItems={howWeWorkItems} /> */}
-      {/* <BusinessNeedsSection slides={slides} /> */}
-      {/* <HighlightsSection /> */}
-      {/* <SeeAlsoSection cards={cards} /> */}
-      {/* <ContactPeople /> */}
+      <HowWeWorkSection howWeWorkItems={howWeWorkItems} />
+      <BusinessNeedsSection slides={slides} />
+      <HighlightsSection />
+      <SeeAlsoSection cards={cards} />
+      <ContactPeople />
     </>
   );
 }
